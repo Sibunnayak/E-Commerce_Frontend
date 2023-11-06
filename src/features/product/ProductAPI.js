16d20e51830cf546dea18d777ccfc757cbe61618
Sibunnayak <sibunnayak8480@gmail.com>
2023-11-07 01:15:19 +0530

    Component Division and filter & sort changes

--- src/features/product/ProductAPI.js
+++ src/features/product/ProductAPI.js
@@ -9,12 +9,21 @@ export function fetchAllProducts() {
   );
 }
 
-export function fetchProductByFilters(filter) {
-//filter = {"category":"smartphone"}
+export function fetchProductByFilters(filter,sort) {
+// filter = {"category":["smartphone","laptops"]}
+// sort = {_sort:"price",_order="desc"}
+// TODO : on server we will support multi values in filter
 let queryString ='';
   for(let key in filter){
-    queryString +=`${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
   }
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+  }	 
 
   return new Promise(async (resolve) =>{
     //TODO: we will not hard-code server URL here
--- src/features/product/ProductSlice.js
+++ src/features/product/ProductSlice.js
@@ -17,8 +17,8 @@ export const fetchAllProductsAsync = createAsyncThunk(
 
 export const fetchProductByFiltersAsync = createAsyncThunk(
   'product/fetchProductByFilters',
-  async (filter) => {
-    const response = await fetchProductByFilters(filter);
+  async ({filter,sort}) => {
+    const response = await fetchProductByFilters({filter,sort});
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
